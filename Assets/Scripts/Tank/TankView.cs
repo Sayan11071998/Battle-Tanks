c6bab87404f29a9ec6bda719168b44f0a7dcdcc2
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-15 19:46:46 +0530

    Tank Movement and Rotation Added

--- Assets/Scripts/Tank/TankView.cs
+++ Assets/Scripts/Tank/TankView.cs
@@ -5,9 +5,39 @@ using UnityEngine;
 public class TankView : MonoBehaviour
 {
     private TankController tankController;
-    
+    public Rigidbody rb;
+
+    private float movement;
+    private float rotation;
+
+    private void Update()
+    {
+        Movement();
+
+        if (movement != 0)
+        {
+            tankController.Move(movement, 30);
+        }
+
+        if (rotation != 0)
+        {
+            tankController.Rotate(rotation, 20);
+        }
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
+    }
+
     public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
     }
 }
