9545083339b2e929b6192482995ac2738df1e364
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-15 23:58:56 +0530

    Unity UI Package Installed

---
+++ Assets/Scripts/UI/TankSelectionUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelectionUI : MonoBehaviour
+{
+    public void BlueTankSelected()
+    {}
+
+    public void GreenTankSelected()
+    {}
+
+    public void RedTankSelected()
+    {}
+}
