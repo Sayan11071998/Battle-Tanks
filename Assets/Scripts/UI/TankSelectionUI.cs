5d9ff17c5030fc9484e75c38a5b953e2e692a8ec
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-16 00:56:19 +0530

    Tank Selection UI Created

--- Assets/Scripts/UI/TankSelectionUI.cs
+++ Assets/Scripts/UI/TankSelectionUI.cs
@@ -1,15 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TankSelectionUI : MonoBehaviour
 {
-    public void BlueTankSelected()
-    {}
+    public TankSpawner tankSpawner;
 
     public void GreenTankSelected()
-    {}
+    {
+        tankSpawner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+    }
+
+    public void BlueTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+    }
 
     public void RedTankSelected()
-    {}
+    {
+        tankSpawner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
+    }
 }
